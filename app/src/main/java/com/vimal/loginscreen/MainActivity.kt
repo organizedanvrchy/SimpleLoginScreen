9b919ce38edb33ad7600e957aee4cc7e5e32ab49
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-04-27 17:05:38 -0400

    Updated Activity Constraints

--- app/src/main/java/com/vimal/loginscreen/MainActivity.kt
+++ app/src/main/java/com/vimal/loginscreen/MainActivity.kt
@@ -3,24 +3,25 @@ package com.vimal.loginscreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vimal.loginscreen.ui.theme.LoginscreenTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            LoginScreen()
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.systemBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+
         }
     }
 }
